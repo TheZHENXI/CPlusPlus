df81159ef1cff854f43283b8d1a811b1fc973cad
TheZHENXI <111845931+TheZHENXI@users.noreply.github.com>
2022-11-04 23:24:50 +0800

    类的定义

---
+++ test2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+public:
+    size_t _age;
+    size_t _id;
+    char _name[20] = "zhang";
+public:
+    void ShowInfo()
+    {
+        cout << _age << endl;
+        cout << _name << endl;
+        cout << _id << endl;
+    }
+    size_t GetAge()
+    {
+        return _age;
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1._age = 20;
+    // s1._name = "zhang";
+    s1._id = 8;
+    s1.ShowInfo();
+    
+    cout << "hello world" << endl;
+    return 0;
+}
